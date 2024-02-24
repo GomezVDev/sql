33ea2d856fb9ef04a51ca726f1fbe58112613c8f
GomezValentin <gomezvalentin.dev@gmail.com>
2024-02-24 20:29:58 -0300

    Problema LEFT JOIN

---
+++ 00_sql_problem.sql
@@ -0,0 +1,6 @@
+#https://leetcode.com/problems/combine-two-tables/
+# Write your MySQL query statement below
+SELECT firstName,lastName,city,state
+FROM Person
+LEFT JOIN Address
+    ON Person.personId = Address.personID;
